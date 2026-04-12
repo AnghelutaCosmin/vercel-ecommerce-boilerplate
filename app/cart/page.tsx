e3a78dacf6d330461b5670bd56181d586cff41f1
Cosmin Angheluta <co.angheluta@reply.it>
2026-04-12 21:29:59 +0200

    feat: added cart and search pages placeholders

---
+++ app/cart/page.tsx
@@ -0,0 +1,8 @@
+export default function CartPage() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center font-sans">
+      <h1 className="text-5xl font-bold mb-4">Cart Page</h1>
+      <p className="text-lg text-gray-400">This is the cart page.</p>
+    </div>
+  );
+}
---
+++ app/search/page.tsx
@@ -0,0 +1,8 @@
+export default function SearchPage() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center font-sans">
+      <h1 className="text-5xl font-bold mb-4">Search Page</h1>
+      <p className="text-lg text-gray-400">This is the search page.</p>
+    </div>
+  );
+}
